13ec0d8fa51ac5cb5e10569f9274079d87398c1b
John Silkey <jpsilkjr86@gmail.com>
2017-10-15 03:03:50 -0700

    added .css and bkg-img assets to Portfolio component, according with css-modules rules

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -2,21 +2,47 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
+import { string } from 'prop-types';
+
+// children components
+import Header from './Portfolio/Header';
+import Main from './Portfolio/Main';
+import Footer from './Portfolio/Footer';
+
+import styles from './Portfolio.css';
+
 // declares Portfolio component as ES6 class, which will be this file's export
 class Portfolio extends Component {
-	constructor() {
-		super();
+  // declares initial state without constructor
+	state = {
+		foo: 'bar'
+	}
 
-		// set initial state
-    this.state = {
-			foo: 'bar'
-		};
+	// defaultProps and propTypes at top of component for easy reference
+	static defaultProps = {
+    fizz: 'fazz',
+    hey: 'hi'
+  }
+  static propTypes = {
+    fizz: string.isRequired,
+    hey: string
+  }
+	// uses arrow function for defining handlers to avoid need for binding *this*
+	handleClick = e => {
+		console.log(this);
+		console.log(e.target);
 	}
 
 	render() {
+		console.log(this.props);
 		return (
-	    <div>
-				<h1>{this.state.foo}</h1>
+	    <div className={styles.kaohsiung}>
+				<Header/>
+				<Main
+					foo={this.state.foo}
+					handleClick={this.handleClick}
+				/>
+				<Footer />
 	    </div>
 		);
 	}
