b1f313f45bd2291b26a967fd3e666e86f52e37d2
John Silkey <jpsilkjr86@gmail.com>
2017-10-16 06:17:50 -0700

    added starter grid to Main

--- app/components/PageTheme/Portfolio/Main.jsx
+++ app/components/PageTheme/Portfolio/Main.jsx
@@ -2,15 +2,24 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import { Button } from 'react-bootstrap';
+import { Grid, Row, Col, Button } from 'react-bootstrap';
 
 
 const Main = ({foo}) => (
-  <div>
-    <h1>Hello, {foo}!</h1>
-    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <p><Button bsStyle="primary">Learn more</Button></p>
-  </div>
+  <Grid>
+  	<Row>
+  		<Col xs={12} sm={6}>
+  			<h1>Hello, {foo}!</h1>
+		    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+		    <p><Button bsStyle="primary">Learn more</Button></p>
+  		</Col>
+  		<Col xs={12} sm={6}>
+  			<h1>Hello, {foo}!</h1>
+		    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+		    <p><Button bsStyle="primary">Learn more</Button></p>
+  		</Col>
+    </Row>
+  </Grid>
 );
 
 export default Main;
