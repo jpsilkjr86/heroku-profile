84d2b1dc1e50929cf0849100e14907307c2e4256
John Silkey <jpsilkjr86@gmail.com>
2017-10-21 02:08:46 -0700

    encapsulated wrapping presentational component GridRow in Main. changed PortfolioNav to only 4 columns on smallSizeAndUp

--- app/components/Portfolio/Main.jsx
+++ app/components/Portfolio/Main.jsx
@@ -6,39 +6,36 @@ import { Grid, Row, Col, Panel, Button } from 'react-bootstrap';
 
 import Media from 'react-media';
 
-import ProjectNav from './Main/ProjectNav';
+import PortfolioNav from './Main/PortfolioNav';
 import AboutMe from './Main/AboutMe';
 import Games from './Main/Games';
 
+const GridRow = ({children}) => <main><Grid><Row>{children}</Row></Grid></main>;
 
 const Main = props => (
-	<main>
-		<Grid>
-			<Row>
-		    <Media query="(min-width: 769px)">
-		      { smallSizeAndUp => smallSizeAndUp
-		      	// always display ProjectNav as side-nav on smallSizeAndUp screens
-		        ? <div>
-		        		<Route path="/" render={()=> <Col sm={6}><ProjectNav/></Col>}/>
-			        	<Col sm={6}>
-			            <Switch>
-			              <Route path="/about" component={AboutMe}/>
-			              <Route path="/games" component={Games}/>
-			              <Redirect exact from="/" to="/about"/>
-			            </Switch>
-			          </Col>
-		          </div>
-		        // for small screens, only display nav at path="/"
-		        : <Switch>
-					      <Route exact path="/" render={()=> <Col xs={12}><ProjectNav/></Col>}/>
-	              <Route path="/about" render={()=> <Col xs={12}><AboutMe/></Col>}/>
-	              <Route path="/games" render={()=> <Col xs={12}><Games/></Col>}/>
-		          </Switch>
-		      }
-		    </Media>
-		  </Row>
-	  </Grid>
-  </main>
+	<GridRow>
+    <Media query="(min-width: 769px)">
+      { smallSizeAndUp => smallSizeAndUp
+      	// always display PortfolioNav as side-nav on smallSizeAndUp screens
+        ? <div>
+        		<Route path="/" render={()=> <Col sm={4}><PortfolioNav/></Col>}/>
+	        	<Col sm={8}>
+	            <Switch>
+	              <Route path="/about" component={AboutMe}/>
+	              <Route path="/games" component={Games}/>
+	              <Redirect exact from="/" to="/about"/>
+	            </Switch>
+	          </Col>
+          </div>
+        // for small screens, only display nav at path="/"
+        : <Switch>
+			      <Route exact path="/" render={()=> <Col xs={12}><PortfolioNav/></Col>}/>
+            <Route path="/about" render={()=> <Col xs={12}><AboutMe/></Col>}/>
+            <Route path="/games" render={()=> <Col xs={12}><Games/></Col>}/>
+          </Switch>
+      }
+    </Media>
+	</GridRow>
 );
 
 export default Main;
