06a4eb795ed59aa11054a21f7e86588fe7389870
John Silkey <jpsilkjr86@gmail.com>
2017-10-20 21:05:46 -0700

    moved routing logic from ProjectNav to Main. changed routing such that ProjectNav only dispays as side-nav for small screens and up (not mobile screens)

--- app/components/Portfolio/Main.jsx
+++ app/components/Portfolio/Main.jsx
@@ -7,12 +7,37 @@ import { Grid, Row, Col, Panel, Button } from 'react-bootstrap';
 import Media from 'react-media';
 
 import ProjectNav from './Main/ProjectNav';
+import AboutMe from './Main/AboutMe';
+import Games from './Main/Games';
+
 
 const Main = props => (
-  <main>
-    <Grid>
-    	<Route path="/" component={ProjectNav}/>
-    </Grid>
+	<main>
+		<Grid>
+			<Row>
+		    <Media query="(min-width: 769px)">
+		      { smallSizeAndUp => smallSizeAndUp
+		      	// always display ProjectNav as side-nav on smallSizeAndUp screens
+		        ? <div>
+		        		<Route path="/" render={()=> <Col sm={6}><ProjectNav/></Col>}/>
+			        	<Col sm={6}>
+			            <Switch>
+			              <Route path="/about" component={AboutMe}/>
+			              <Route path="/games" component={Games}/>
+			              <Redirect exact from="/" to="/about"/>
+			            </Switch>
+			          </Col>
+		          </div>
+		        // for small screens, only display nav at path="/"
+		        : <Switch>
+					      <Route exact path="/" render={()=> <Col xs={12}><ProjectNav/></Col>}/>
+	              <Route path="/about" render={()=> <Col xs={12}><AboutMe/></Col>}/>
+	              <Route path="/games" render={()=> <Col xs={12}><Games/></Col>}/>
+		          </Switch>
+		      }
+		    </Media>
+		  </Row>
+	  </Grid>
   </main>
 );
 
--- app/components/Portfolio/Main/ProjectNav.jsx
+++ app/components/Portfolio/Main/ProjectNav.jsx
@@ -2,41 +2,14 @@
 import React from 'react';
 import { Switch, Route, Redirect, Link } from 'react-router-dom';
 
-import { Grid, Row, Col, Panel, Button } from 'react-bootstrap';
-
-import Media from 'react-media'
-
-import AboutMe from './AboutMe';
-import Games from './Games';
-
+import { Panel, Button } from 'react-bootstrap';
 
 const ProjectNav = props => (
-	<Row>
-    <Col xs={12} sm={6}>
-      <Panel>
-		    <h1>Hello!</h1>
-		    <p>Project Nav</p>
-		    <p><Button bsStyle="primary">Learn more</Button></p>
-		  </Panel>
-    </Col>
-    <Media query="(min-width: 769px)">
-      { smallSizeAndUp => smallSizeAndUp
-        ? <Col xs={12} sm={6}>
-            <Switch>
-              <Route path="/about" component={AboutMe}/>
-              <Route path="/games" component={Games}/>
-              <Redirect exact from="/" to="/about"/>
-            </Switch>
-          </Col>
-        : <Col xs={12} sm={6}>
-            <Switch>
-              <Route path="/about" component={AboutMe}/>
-              <Route path="/games" component={Games}/>
-            </Switch>
-          </Col>
-      }
-    </Media>
-  </Row>
+	<Panel>
+    <h1>Hello!</h1>
+    <p>Project Nav</p>
+    <p><Button bsStyle="primary">Learn more</Button></p>
+  </Panel>
 );
 
 export default ProjectNav;
