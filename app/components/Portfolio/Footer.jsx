d849daed613cfa0e6b5c54b6598afc9b1c6fe03b
John Silkey <jpsilkjr86@gmail.com>
2017-10-21 02:06:43 -0700

    emptied content for now

--- app/components/Portfolio/Footer.jsx
+++ app/components/Portfolio/Footer.jsx
@@ -6,9 +6,7 @@ import {  Button } from 'react-bootstrap';
 
 const Footer = () => (
   <footer className="footer">
-    <h1>Hello, world!</h1>
-    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <p><Button bsStyle="primary">Learn more</Button></p>
+    
   </footer>
 );
 
