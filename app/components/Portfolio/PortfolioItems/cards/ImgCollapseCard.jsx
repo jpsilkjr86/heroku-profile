6b735581a72072575259ce73cde786a2671cd77c
John Silkey <jpsilkjr86@gmail.com>
2017-10-24 12:43:07 -0700

    finished working prototype of ImgCollapseCard

--- app/components/Portfolio/PortfolioItems/cards/ImgCollapseCard.jsx
+++ app/components/Portfolio/PortfolioItems/cards/ImgCollapseCard.jsx
@@ -1,13 +1,15 @@
 // imports react component classes
 import React, { Component } from 'react';
 
-import { Col } from 'react-bootstrap';
+import { Glyphicon, Button, ButtonGroup } from 'react-bootstrap';
 
 import { Collapse } from 'reactstrap';
 
 import Card from './Card.jsx';
 
 import styles from './ImgCollapseCard.css';
+
+import githubIcon from '../img/github.svg';
 
 class ImgCollapseCard extends Component {
 	state = {
@@ -21,7 +23,7 @@ class ImgCollapseCard extends Component {
 
 	render() {
 			
-		const { imgSrc, link, subtext, children } = this.props;
+		const { imgSrc, title, githubLink, deployedLink, description } = this.props;
 		const { isOpen } = this.state;
 
 		return (
@@ -31,13 +33,80 @@ class ImgCollapseCard extends Component {
 		    </Collapse>
 		    <Collapse isOpen={!isOpen}>
 			    <div className={styles.content} onClick={this.toggle}>
-			    	{children}
+			    	<h4>{title}</h4>
+			    	{description}
 			    </div>
 		    </Collapse>
-		    <div className={styles.subtext}>{subtext}</div>
+		    <div className={styles.subtext}>
+	    	  <ButtonGroup justified>
+				    <Button href="#" bsStyle="default" onClick={this.toggle}>
+				    	<Glyphicon glyph="plus"/>
+				    </Button>
+				    <Button href={githubLink} bsStyle="default" target="_blank">
+				    	<img src={githubIcon} className={styles["btn-img"]}/>
+				    </Button>
+				    <Button href={deployedLink} bsStyle="default" target="_blank">
+				    	<Glyphicon glyph="link"/>
+				    </Button>
+				  </ButtonGroup>
+		    </div>
 		  </div>
 		);
 	}
 }
 
 export default ImgCollapseCard;
+/*
+<Row>
+		    		<Col xs={4}>
+				    	<Button href="#" bsStyle="default"><Glyphicon glyph="plus"/></Button>
+			    	</Col>
+		    		<Col xs={4}>
+				    	<Button href="#" bsStyle="default"><img src={github} className={styles["btn-img"]}/></Button>
+			    	</Col>
+		    		<Col xs={4}>
+				    	<Button href="#" bsStyle="default"><Glyphicon glyph="link"/></Button>
+			    	</Col>
+			    </Row>
+
+<ButtonGroup justified>
+				    <Button href="#" bsStyle="success"><Glyphicon glyph="plus"/></Button>
+				    <Button href="#" bsStyle="success"><img src={github} className={styles["btn-img"]}/></Button>
+				    <Button href="#" bsStyle="success"><Glyphicon glyph="link"/></Button>
+				  </ButtonGroup>
+<Row>
+		    		<Col xs={4}>
+				    	<Dropdown id="links">
+					      <Dropdown.Toggle bsStyle="success">
+					        <Glyphicon glyph="link" />
+					      </Dropdown.Toggle>
+					      <Dropdown.Menu>
+					        <MenuItem eventKey="1">GitHub Repo</MenuItem>
+					        <MenuItem eventKey="2">Deployed App</MenuItem>
+					      </Dropdown.Menu>
+					    </Dropdown>
+			    	</Col>
+		    		<Col xs={4}>
+				    	<Dropdown id="links">
+					      <Dropdown.Toggle bsStyle="success">
+					        <Glyphicon glyph="link" />
+					      </Dropdown.Toggle>
+					      <Dropdown.Menu>
+					        <MenuItem eventKey="1">GitHub Repo</MenuItem>
+					        <MenuItem eventKey="2">Deployed App</MenuItem>
+					      </Dropdown.Menu>
+					    </Dropdown>
+			    	</Col>
+		    		<Col xs={4}>
+				    	<Dropdown id="links">
+					      <Dropdown.Toggle bsStyle="success">
+					        <Glyphicon glyph="link" />
+					      </Dropdown.Toggle>
+					      <Dropdown.Menu>
+					        <MenuItem eventKey="1">GitHub Repo</MenuItem>
+					        <MenuItem eventKey="2">Deployed App</MenuItem>
+					      </Dropdown.Menu>
+					    </Dropdown>
+			    	</Col>
+			    </Row>
+			    */
