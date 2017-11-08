a941e03d75fe24ff5a7609014b79516feb7c5a5c
John Silkey <jpsilkjr86@gmail.com>
2017-11-07 21:48:37 -0700

    added NoMatchRoute for displaying 404 as default route

--- app/components/Portfolio/Main.jsx
+++ app/components/Portfolio/Main.jsx
@@ -2,13 +2,26 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import { Grid, Row, Col, Clearfix } from 'react-bootstrap';
+import { Grid, Row, Col, Clearfix, Button } from 'react-bootstrap';
 
 import Media from 'react-media';
 
 import PortfolioNav from './PortfolioNav';
+import InfoCard from './PortfolioItems/cards/InfoCard';
 
+// wrapping grid and row
 const GridRow = ({children}) => <main><Grid><Row>{children}</Row></Grid></main>;
+
+// NoMatchRoute for displaying 404
+const NoMatchRoute = () => (
+  <Route render={props => {console.log(props); return (
+    <InfoCard header="404: Page Not Found">
+      <Button onClick={props.history.goBack}>Back</Button>
+      &nbsp;&nbsp;
+      <Button onClick={()=> props.history.push('/')}>Home</Button>
+    </InfoCard>
+  )}}/>    
+);
 
 const Main = ({ routes }) => (
 	<GridRow>
@@ -30,6 +43,8 @@ const Main = ({ routes }) => (
         	<Route path={path} key={path} component={Component}/>
         ))}
         <Redirect exact from="/" to={routes[0].path}/>
+        {/* 404 route put last in the Switch to ensure it is rendered as default */}
+        <NoMatchRoute/>
       </Switch>
     </Col>
 	</GridRow>
