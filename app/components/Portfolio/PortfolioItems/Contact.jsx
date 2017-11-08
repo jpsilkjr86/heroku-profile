9db02b140186dadd42265602624bcb86ab9351f9
John Silkey <jpsilkjr86@gmail.com>
2017-11-08 01:15:21 -0700

    fixed layout of Contact.jsx so it's divided into rows and columns

--- app/components/Portfolio/PortfolioItems/Contact.jsx
+++ app/components/Portfolio/PortfolioItems/Contact.jsx
@@ -3,24 +3,39 @@ import React from 'react';
 
 import InfoCard from './cards/InfoCard';
 
+import { Row, Col } from 'react-bootstrap';
+
 const resumeLink = 'https://docs.google.com/document/d/1bn5rhGN1l2Hli8Ix3WOvoFqV3xSklmLDtQoadtWE26g';
 const linkedinLink = 'https://www.linkedin.com/in/john-silkey-jr';
 const githubLink = 'https://github.com/jpsilkjr86';
 
 import { ResumeIcon, LinkedInIcon, GitHubIcon } from './contact-items';
 
+const ContactRow = ({contactType, contactValue}) => (
+	<Row>
+		<Col xs={3} sm={2}>{contactType}</Col>
+		<Col xs={9} sm={10}>{contactValue}</Col>
+	</Row>
+);
+
 const Contact = props => (
   <InfoCard header="Contact Me" linkIcons={[GitHubIcon, LinkedInIcon, ResumeIcon]}>
-  	<p>
-  		Email: <a href="mailto:jpsilkjr86@gmail.com">jpsilkjr86@gmail.com</a>
-  		<br/>
-  		Phone: (see <a href={resumeLink} target="_blank">Resume</a>)
-  		<br/>
-  		LinkedIn: <a href={linkedinLink} target="_blank">{linkedinLink}</a>
-  		<br/>
-  		GitHub: <a href={githubLink} target="_blank">{githubLink}</a>
-  		<br/>
-  	</p>
+		<ContactRow
+			contactType="Email:"
+			contactValue={<a href="mailto:jpsilkjr86@gmail.com">jpsilkjr86@gmail.com</a>}
+		/>
+		<ContactRow
+			contactType="Phone:"
+			contactValue={<span>(see <a href={resumeLink} target="_blank">Resume</a>)</span>}
+		/>
+		<ContactRow
+			contactType="LinkedIn:"
+			contactValue={<a href={linkedinLink} target="_blank">{linkedinLink}</a>}
+		/>
+		<ContactRow
+			contactType="GitHub:"
+			contactValue={<a href={githubLink} target="_blank">{githubLink}</a>}
+		/>
   </InfoCard>
 );
 
